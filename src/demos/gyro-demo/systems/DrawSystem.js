74c4693e8bf551d14a3d3c96f9fa41c11a7f0d48
yintothayang <yintothayang@gmail.com>
2017-01-21 21:11:34 -0500

    add gyro stuff

---
+++ src/demos/gyro-demo/systems/DrawSystem.js
@@ -0,0 +1,29 @@
+import {Ludic} from 'Ludic'
+import {BaseSystem} from 'ein'
+export default class DrawSystem extends BaseSystem {
+  constructor(){
+    super();
+  }
+
+  //Overide
+  onEntityAdded(manager){
+    this.entities = manager.getEntitiesByClassName("Ball");
+  }
+
+  onEntityRemoved(manager){}
+
+  onSystemAddedTo(manager){
+    this.em = manager;
+  }
+
+  onSystemRemovedFrom(manager){}
+
+  //Overide
+  update(...args){
+    Ludic.context.save();
+    this.entities.forEach(entity => {
+      entity.draw(Ludic.context);
+    });
+    Ludic.context.restore();
+  }
+};
